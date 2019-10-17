e9552c70476b89a7a6f650eb6f7f0a98b8815c35
Elke Burgers <elke.burgers@hotmail.com>
2019-10-17 10:40:27 -0300

    HTML do header, criaçao dos arquivos blog e noticias, correção dos nomes das imagens de logo.

--- header.php
+++ header.php
@@ -1,31 +1,66 @@
-<!-- Emilia: página para criação do header padrão para todas as outras páginas do portal. Fazer esta e o footer primeiros, por favor. -->
-<!-- Header copiado da loja virtual da aula, caso ajude ter a base. Pode apagar caso prefira fazer do zero  -->
-<!-- Emilia, pecisei limpar algumas coisas do header original da loja, assim como nas variáveis, pois estava dando muito erro para as outras páginas. Ficou só o header com o login e navbar original da loja virtual. -->
+<!-- header padrão para todas as outras páginas do portal.-->
 
 <?php include_once("config/variaveis.php") ?>
 
-<header class="navbar">
-        <h1 id=logo>
-            <?php echo $nomeSistema; ?>
-        </h1>
-        <nav>
+<header>
+    <section class="navbar">
+    <img src="imagens/Logo_degrade.png" alt="logo portal do surf">
+    <form class="form-inline my-2 my-lg-0 justify-contents-center align-items-center">
+        <input class="form-control mr-sm-2" type="search" placeholder="" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Busca</button>
+    </form>
+    <nav>
+        <ul class="nav">
+            <?php if(isset($usuario) && $usuario != []) {?>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Olá <?php echo $usuario["nome"]; ?></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Sair</a>
+            </li>
+            <?php } else { ?>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Cadastro</a>
+            </li>
+            <?php } ?>
+        </ul>
+    </nav>
+            </section>
+    <section class="container mt-3">
+        <nav class="nav-bar">
             <ul class="nav">
-                <?php if(isset($usuario) && $usuario != []) {?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Olá <?php echo $usuario["nome"]; ?></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Sair</a>
-                    </li>
-                <?php } else { ?>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Cadastro</a>
-                    </li>
-                <?php } ?>   
-            </ul>
+                <li class="nav-item">
+                    <a class="nav-link" href="previsao.php">Previsão</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="caronas.php">Carona</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="desapego.php">Desapego</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="encontre.php">Encontre</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="blog.php">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="noticias.php">Notícias</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Links</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="https://www.isasurf.org/">ISA</a>
+                        <a class="dropdown-item" href="https://www.worldsurfleague.com/">World Surf League</a>
+                        <a class="dropdown-item" href="http://wsllatinamerica.com/">WSL Latin America</a>
+                        <a class="dropdown-item" href="http://www.abrasp.com/">ABRASP</a>
+                        <a class="dropdown-item" href="https://magicseaweed.com/">Magic Sea Weed</a>
+                    </div>
+                </li>
         </nav>
-    </header>
-   
+    </section>
+</header>
