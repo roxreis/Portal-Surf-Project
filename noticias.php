69f20710df35712ee1bc5600a013af6231e6b046
Elke Burgers <elke.burgers@hotmail.com>
2019-10-24 09:52:39 -0300

    Criação de cards provisórios para os arquivos blog.php e noticias.php.

--- noticias.php
+++ noticias.php
@@ -1,17 +1,48 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="css/style.css" rel="stylesheet">
     <title>Portal do Surf - Notícias</title>
 </head>
-<body>
-    <?php include_once("padrao/header.php") ?>   
 
-    <?php include_once("padrao/footer.php") ?>   
+<body>
+    <?php include_once("padrao/header.php") ?>
+
+    <div class="container mt-5 mb-5">
+        <h2 class="mb-5 text-info">Saiba tudo sobre o mundo do surf!</h2>
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Medina cortou a unha</h4>
+                <p class="card-text">Estava insuportável surfar com elas daquele tamanho. Até a minha namorada reclamou. E se você Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi, quasi sapiente quod repellendus deserunt magnam laborum magni ...</p>
+                <a href="#" class="card-link">Saiba mais</a>
+            </div>
+        </div>
+        <br>
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Mineirinho ficou com inveja</h4>
+                <p class="card-text">Eu queria ter tanta repercursão na mídia quanto o Medina. Até quando ele corta a unha vira notícia! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam officiis vero nihil exercitationem animi id nobis, sunt, rep...</p>
+                <a href="#" class="card-link">Saiba mais</a>
+            </div>
+        </div>
+        <br>
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Festival de super tubos em Peniche/Portugal com galera do Amazonas</h4>
+                <p class="card-text">Galera amazonense surfista de pororocado Amazonas se encontra em Peniche, Portugal, para curtir e participar o campeonato de super tubos. A praia é  conhecida no mundo por ser um dos principais picos deste tipo de onda...</p>
+                <a href="#" class="card-link">Saiba mais</a>
+            </div>
+        </div>
+    </div>
+
+
+    <?php include_once("padrao/footer.php") ?>
 </body>
+
 </html>
