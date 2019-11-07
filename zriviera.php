b6f8733f55bb264096ea986b98fe50e7f1da6eb7
Felipe Siqueira <felipesiqueira1212@gmail.com>
2019-11-07 17:30:43 -0200

    Página da previsão e primeira praia iniciadas no responsivo

--- zriviera.php
+++ zriviera.php
@@ -11,33 +11,37 @@
     <?php include_once("padrao/header.php") ?>   
     <div class=container>
             <h1>PRAIA DE RIVIERA</h1>
-            <nav>
+            <nav class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <ul class="nav justify-content-between">
                     <li class="nav-link"><a href="previsao.php">VOLTAR</a></li>
                     <li class="nav-link"><a href="zbarradouna.php">PROXIMA PRAIA</a></li>
                 </ul>
             </nav>
-            <div class="d-flex flex-row mr-3">
-                <img src="imagens/img-previsao/riviera.png" class="img-fluid" style="width: 400px;" alt="responsive image">
-                <div class="container ml-3">
-                    <p>As melhores valas encontram-se nos dois extremos, sobretudo no canto direito.</p>
-                    <ul>
-                        <li>Tamanho - 2 a 6 pés</li>
-                        <li>Ondulação ideal - Sudeste | Leste | Sul</li>
-                        <li>Vento ideal - Quadrante Norte</li>
-                        <li>Grau de dificuldade - Baixa</li>
-                        <li>Melhor época - Outono | Primavera</li>
-                        <li>Constância - Alta</li>
-                        <li>Crowd - Intenso</li>
-                    </ul>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-5 col-lg-6 col-md-12 col-sm-12">
+                        <img src="imagens/img-previsao/riviera.png" class="img-fluid mb-3" stylealt="responsive image">
+                    </div>
+                    <div class="col-xl-7 col-lg-6 col-md-12 col-sm-12">
+                        <p>As melhores valas encontram-se nos dois extremos, sobretudo no canto direito.</p>
+                        <ul>
+                            <li>Tamanho - 2 a 6 pés</li>
+                            <li>Ondulação ideal - Sudeste | Leste | Sul</li>
+                            <li>Vento ideal - Quadrante Norte</li>
+                            <li>Grau de dificuldade - Baixa</li>
+                            <li>Melhor época - Outono | Primavera</li>
+                            <li>Constância - Alta</li>
+                            <li>Crowd - Intenso</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div class="container mt-2">
-                <h3>Condições atuais:</h3>
+                <h4>Condições atuais:</h4>
                 <img src="imagens/img-previsao/prevdia.png" class="img-fluid" alt="condicoes atuais">
             </div>
             <div class="container p-0 mt-2">
-                <h2>Encontre nesta área:</h2>
+                <h4>Encontre nesta área:</h4>
                 <select class="form-control font-accent tt-input mx-80" name="encontre">
                     <option disabled selected>Encontre:</option>
                     <option value=”una”>Acomodação</option>
@@ -51,7 +55,7 @@
             </div>
         </div>
         <div class=container>
-            <h2>Itens a venda nesta área:</h2>
+            <h4>Itens a venda nesta área:</h4>
             <div class="row justify-content-between">
                 <img src="imagens/img-previsao/venda1.png" class="img-fluid" alt="condicoes atuais">
                 <img src="imagens/img-previsao/venda1.png" class="img-fluid" alt="condicoes atuais">
@@ -59,20 +63,18 @@
             </div>
         </div>
         <div class="container mt-2">
-            <h3>Previsão dos próximos 7 dias:</h3>
+            <h4>Previsão dos próximos 7 dias:</h4>
             <img src="imagens/img-previsao/previsurf.png" class="img-fluid" alt="condicoes atuais">
         </div>
-        <div class="container mt-2">
-            <h3>Pra quem manja:</h3>
-            <img src="imagens/img-previsao/manja.png" class="img-fluid" alt="condicoes atuais">
+        <div class="container justify-content-center mt-2">
+            <h4>Pra quem manja:</h4>
+            <img src="imagens/img-previsao/manja.png" class="img-fluid mx-3" style="vw: 100%;"alt="condicoes atuais">
         </div>
-        <div class=container>
-            <h2>Fotos da galera:</h2>
-            <div class=row>
-                <img src="imagens/img-previsao/postai.png" class="img-fluid mx-1" alt="condicoes atuais">
-                <img src="imagens/img-previsao/postai.png" class="img-fluid mx-1" alt="condicoes atuais">
-            </div>
+        <div class="container mt-2">
+            <h4>Fotos da galera:</h4>
+                <img src="imagens/img-previsao/fotosgalera.png" class="img-fluid mx-1" alt="condicoes atuais">
         </div>
     <?php include_once("padrao/footer.php") ?>   
 </body>
 </html>
+
