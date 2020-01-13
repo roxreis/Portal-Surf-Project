348776e37f157bf342d5ea0e97da7aabe46de779
Elke Burgers <elke.burgers@hotmail.com>
2020-01-13 12:08:47 -0300

    alinhamento de campos da view cadastro de usuario

--- resources/views/usuarioCadastro.blade.php
+++ resources/views/usuarioCadastro.blade.php
@@ -8,7 +8,7 @@ Cadastro Usuario
 
 <main class="mt-5">
     <section class="container mt-5 shadow-lg p-3 cardBlog" id="usuarioTopo">
-        <form action="/usuarioCadastro" method="GET" enctype="multipart/form-data" class="mt-3">
+        <form action="/usuarioCadastro" method="GET" enctype="multipart/form-data" class="mt-3 ml-5">
         @CSRF
             <!-- dados pessoais -->
 
@@ -31,22 +31,22 @@ Cadastro Usuario
             <br>
 
             <div class="form-inline mt-3">
-                <label for="name" class="mr-3">Nome completo:</label>
-                <input type="text" class="form-control col-sm-10" id="name" name="name">
-            </div>
+                <label for="name" class="col-sm-1">Nome completo:</label>
+                <input type="text" class="form-control col-sm-9" id="name" name="name">
+            </div><br>
 
             <div class="form-inline mt-3">
-                <label for="email" class="mr-3">E-mail:</label>
+                <label for="email" class="col-sm-1">E-mail:</label>
                 <input type="email" class="form-control col-sm-4" id="email" name="email">
-                <label for="password" class="ml-3 mr-3">Senha:</label>
+                <label for="password" class="col-sm-1">Senha:</label>
                 <input type="password" class="form-control col-sm-4" id="password" name="password">
             </div>
             <br>
 
             <div class="form-inline mt-3">
-                <label for="cpf" class="mr-4">CPF:</label>
+                <label for="cpf" class="col-sm-1">CPF:</label>
                 <input type="text" class="form-control col-sm-4" id="cpf" name="cpf">
-                <label for="gender" class="ml-3 mr-3">Gênero:</label>
+                <label for="gender" class="col-sm-1">Gênero:</label>
                 <select class="form-control col-sm-4" id="gender" name="gender">
                     <option value="feminino">Feminino</option>
                     <option value="masculino">Masculino</option>
@@ -56,17 +56,17 @@ Cadastro Usuario
             <br>
 
             <div class="form-inline mt-3">
-                <label for="state" class="mr-3">Estado:</label>
+                <label for="state" class="col-sm-1">Estado:</label>
                 <input type="text" class="form-control col-sm-4" id="state" name="state">
-                <label for="city" class="ml-3 mr-3">Cidade:</label>
+                <label for="city" class="col-sm-1">Cidade:</label>
                 <input type="text" class="form-control col-sm-4" id="city" name="city">
             </div>
             <br>
 
             <div class="form-inline mt-3">
-                <label for="phone1" class="mr-3">Telefone 01:</label>
+                <label for="phone1" class="col-sm-1">Telefone 01:</label>
                 <input type="text" class="form-control col-sm-4" id="phone1" name="phone1">
-                <label for="phone2" class="ml-3 mr-3">Telefone 02:</label>
+                <label for="phone2" class="col-sm-1">Telefone 02:</label>
                 <input type="text" class="form-control col-sm-4" id="phone2" name="phone2">
             </div>
             <br><br>
@@ -88,8 +88,8 @@ Cadastro Usuario
             <!-- aceite de termos e condicoes -->
             <div>
                 <div class="form-group">
-                    <label class="font-weight-bold">Termos e Condições</label>
-                    <textarea class="form-control" rows="3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iste necessitatibus ut alias quo suscipit sed excepturi modi! Eos consequuntur eligendi earum ex odit officiis eius corrupti officia delectus ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iste necessitatibus ut alias quo suscipit sed excepturi modi! Eos consequuntur eligendi earum ex odit officiis eius corrupti officia delectus ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iste necessitatibus ut alias quo suscipit sed excepturi modi! Eos consequuntur eligendi earum ex odit officiis eius corrupti officia delectus ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iste necessitatibus ut alias quo suscipit sed excepturi modi! Eos consequuntur eligendi earum ex odit officiis eius corrupti officia delectus ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iste necessitatibus ut alias quo suscipit sed excepturi modi! Eos consequuntur eligendi earum ex odit officiis eius corrupti officia delectus ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iste necessitatibus ut alias quo suscipit sed excepturi modi! Eos consequuntur eligendi earum ex odit officiis eius corrupti officia delectus ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iste necessitatibus ut alias quo suscipit sed excepturi modi! Eos consequuntur eligendi earum ex odit officiis eius corrupti officia delectus ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iste necessitatibus ut alias quo suscipit sed excepturi modi! Eos consequuntur eligendi earum ex odit officiis eius corrupti officia delectus ea! </textarea>
+                    <label class="font-weight-bold">Termos e Condições do Portal do Surf</label>
+                    <textarea class="form-control col-sm-11" rows="5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iste necessitatibus ut alias quo suscipit sed excepturi modi! Eos consequuntur eligendi earum ex odit officiis eius corrupti officia delectus ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iste necessitatibus ut alias quo suscipit sed excepturi modi! Eos consequuntur eligendi earum ex odit officiis eius corrupti officia delectus ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iste necessitatibus ut alias quo suscipit sed excepturi modi! Eos consequuntur eligendi earum ex odit officiis eius corrupti officia delectus ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iste necessitatibus ut alias quo suscipit sed excepturi modi! Eos consequuntur eligendi earum ex odit officiis eius corrupti officia delectus ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iste necessitatibus ut alias quo suscipit sed excepturi modi! Eos consequuntur eligendi earum ex odit officiis eius corrupti officia delectus ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iste necessitatibus ut alias quo suscipit sed excepturi modi! Eos consequuntur eligendi earum ex odit officiis eius corrupti officia delectus ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iste necessitatibus ut alias quo suscipit sed excepturi modi! Eos consequuntur eligendi earum ex odit officiis eius corrupti officia delectus ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iste necessitatibus ut alias quo suscipit sed excepturi modi! Eos consequuntur eligendi earum ex odit officiis eius corrupti officia delectus ea! </textarea>
                 </div>
                 <div class="form-check-inline">
                     <label class="form-check-label mt-3" for="terms">
