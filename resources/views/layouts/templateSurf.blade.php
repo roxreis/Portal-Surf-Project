bc5fb56906e85b85b50b00975ff6f0ff3a0b1272
roxreis <rodrigoassuncao12@gmail.com>
2020-01-15 09:36:29 -0200

    resolve merge

--- resources/views/layouts/templateSurf.blade.php
+++ resources/views/layouts/templateSurf.blade.php
@@ -13,7 +13,7 @@
 <header class="cabecario">
       
       <div id=upHeader>   
-        <a href="/home"><img src="../imagens/Logo_degrade.png" id="logo" alt=""></a>
+''        <a href="/home"><img src="../imagens/Logo_degrade.png" id="logo" alt=""></a>
         <div style="margin-left:356px;">
             <a href="/usuarioCadastro" type="button" class="btn btn-primary" id="botaoHeader1">Cadastro</a>
             <a href="#" class="btn btn-success"id="botaoHeader2">Login</a>
