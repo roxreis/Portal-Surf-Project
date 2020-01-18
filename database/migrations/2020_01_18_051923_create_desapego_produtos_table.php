e57e6cb5556d540656b7f33ff81fb47ca23ce34e
Caio Alves <caiojuodzevicius@gmail.com>
2020-01-18 03:20:36 -0300

    update migrations / desapego

--- database/migrations/2020_01_18_051923_create_desapego_produtos_table.php
+++ database/migrations/2020_01_18_051923_create_desapego_produtos_table.php
@@ -14,21 +14,20 @@ class CreateDesapegoProdutosTable extends Migration
     public function up()
     {
         Schema::create('desapego_produtos', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->bigIncrements('idProdutosDesapego');
+            $table->bigIncrements('idProdutosDesapego')->unique();
             $table->string('segmento');
-            $table->string('tipoEquipamento', 15);
-            $table->string('estadoProduto', 50);
-            $table->string('tituloProduto', 256);
-            $table->string('descricaoProduto', 360);
-            $table->string('precoProduto', 2);
-            $table->string('retiradaEstado', 45);
-            $table->string('retiradaCidade', 45);
-            $table->string('retiradaBairro', 45);
-            $table->string('fotoProduto', 256);
-            $table->bigIncrements('idUsuario');
-            $table->bigIncrements('Usuarios_id');
-            $table->bigIncrements('vendasDesapego_idVendasDesapego');
+            $table->string('typeEquipament', 15);
+            $table->string('stateProduct', 50);
+            $table->string('titleProduct', 256);
+            $table->string('descriptionProduct', 360);
+            $table->string('priceProduct', 8, 2);
+            $table->string('withdrawalState', 45);
+            $table->string('withdrawalCity', 45);
+            $table->string('withdrawalNeighborhood', 45);
+            $table->string('imgProduct', 256)->nullable();
+            $table->integer('idUser')->unique();
+            $table->integer('Users_id')->unique();
+            $table->integer('vendasDesapego_idVendasDesapego')->unique();
             $table->timestamps();
         });
     }
