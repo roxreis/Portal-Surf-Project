11aaa5b46450b2b0a8aedc6f0b75d6f039fa1321
Gustavo Corrêa <guts.correa@gmail.com>
2020-01-18 14:56:06 -0300

    Adicionei rotas, controllers e views das páginas Noticias, Previsão e Blog. As views ainda precisam passar por ajustes posicionais.

---
+++ app/Http/Controllers/PrevisaoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PrevisaoController extends Controller
+{
+  public function viewPrevisao(Request $request){
+      return view('previsao');
+}
+
+}
