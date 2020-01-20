bf1e2ed56a26a7e5a2e421370ddfb1869a1e7b99
Caio Alves <caiojuodzevicius@gmail.com>
2020-01-20 01:36:48 -0300

    Update models / desapego

--- app/Models/Desapego.php
+++ app/Models/Desapego.php
@@ -7,11 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
-<<<<<<< HEAD
-class Desapego extends Model
-{
-    // protected $fillable = ['title', 'description'];
-=======
+
 class Desapego extends Model{
 
     use Notifiable;
@@ -25,5 +21,4 @@ class Desapego extends Model{
             'ofertaSegmento', 'ofertaTipo', 'ofertaEstProduto', 'ofertaTitulo', 'ofertaDescr', 'ofertaPreco', 'ofertaEstado', 'ofertaCidade', 'ofertaBairro', 'imgProduto',
         ];
     }
->>>>>>> 5f6c14cf5a2c77f86e9180d8c4c4dbb27665c26c
-}
+
