f3aaeb3e5d51316c222acb16900f1a807e010c7a
Elke Burgers <elke.burgers@hotmail.com>
2020-02-02 17:40:01 -0300

    ajustes de layout provisórios para encontre - prioriza o API

--- resources/views/encontre.blade.php
+++ resources/views/encontre.blade.php
@@ -1,107 +1,90 @@
 @extends('layouts.templateSurf')
 
 @section('titulo')
-    Encontre
+Encontre
 @endsection
 
 @section('conteudo')
-    <!-- original Elke - inicio -->
-        <!-- <section class="container d-flex justify-contents-center text-info">
-            <div class="col-6">
-                <h1 class="p-3 mt-5 ">Encontre tudo o que você precisa para aproveitar a água!</h1> -->
 
-        <!-- linha do label: modelo de form (form control: select) puxado do bootstrap W3C: https://www.w3schools.com/bootstrap4/tryit.asp?filename=trybs_form_select&stacked=h -->
+<!-- banner -->
+<section class="d-flex">
+    <div class="encontreBanner">
+        <div class="frontBanner">
+            <h1 class="mx-auto caronaTextoBanner">Encontre</h1>
+        </div>
+    </div>
+</section>
 
-        <!--  <div class="container">
-        <form action="/action_page.php">
-                    <div class="form-group col-6 mt-3">
-                        <label>Digite o lugar:</label>
-                        <input type="text" name="place">
-        <label for="sel1">Escolha a cidade:</label>
-                <select class="form-control" id="sel1" name="cidade">
-                    <option></option>
-                    <option>Ubatuba</option>
-                    <option>Cidade 02</option>
-                    <option>Cidade 03</option>
-                    <option>Cidade 04</option>
-                </select>
-                <br>
-                        <label for="sel1">Escolha o serviço (selecione um):</label>
-                            <select class="form-control" id="sel1" name="servico">
-                                <option></option>
-                                <option>Escolas de surf</option>
-                                <option>Shaper</option>
-                                <option>Surfwear</option>
-                                <option>Pranchas</option>
-                                <option>Pousadas</option>  
-                                <option>Restaurantes</option>
-                            </select>
-                        <br>
-                    </div>
-                </form>
-                <button type="submit" class="btn btn-primary">Encontre</button>
+
+
+<section class="container">
+    <!-- titulo da pagina -->
+    <h1 id="encontreTopo" class="mt-5 text-center">Encontre lojas com produtos e serviços de <br> surf aonde
+        você estiver:</h1>
+
+
+    <!-- campos de busca -->
+    <section class="container mt-5">
+        <div class="row align-items-center">
+
+            <div class="encontreSelecao col-md-1 col-sm-1">
+                <h3 class="font-weight-bold">O que você procura?</h3><br>
             </div>
-        </section> -->
-        <!-- original Elke - fim -->
 
-        <!-- banner -->
-        <section class= "d-flex">
-            <div class="encontreBanner">
-                <div class = "frontBanner">
-                    <h1 class="mx-auto caronaTextoBanner">Encontre</h1>
+            <div class="col-lg-4 col-md-4">
+                <div>
+                    <label for="encontreLocal">Selecione um local:</label>
+                    <select class="form-control" id="encontreLocal">
+                        <option disabled selected> </option>
+                        <option id="encontreLocal" value="riviera">Riviera</option>
+                        <option id="encontreLocal" value="maresias">Maresias</option>
+                        <option id="encontreLocal" value="pauba">Paúba</option>
+                        <option id="encontreLocal" value="baleia">Baleia</option>
+                        <option id="encontreLocal" value="barra">Barra do Una</option>
+                        <option id="encontreLocal" value="guaeca">Guaecá</option>
+                        <option id="encontreLocal" value="juquehy">Juquehy</option>
+                        <option id="encontreLocal" value="santiago">Santiago</option>
+                        <option id="encontreLocal" value="cambury">Cambury</option>
+                    </select>
+                </div><br>
+
+                <div>
+                    <div class="column">
+                        <button type="button" class="btn encontreBotao" data-toggle="tooltip" data-placement="top"
+                            title="Surfshop" id="encontreLocal" value="surfshop">Surfshop</button><br><br>
+
+                        <button type="button" class="btn encontreBotao" data-toggle="tooltip" data-placement="top"
+                            title="Surfshop" id="encontreLocal" value="equipamentos">Equipamentos</button><br><br>
+
+                        <button type="button" class="btn encontreBotao" data-toggle="tooltip" data-placement="top"
+                            title="Surfshop" id="encontreLocal" value="aulas">Aulas</button><br><br>
+                    </div>
+
+                    <!-- <select class="form-control" id="encontreServico">
+                    <option disabled selected> </option>
+                    <option id="encontreServico" value=”surfshop”>Surf Shop</option>
+                    <option id="encontreServico" value=”aulas”>Aulas</option>
+                    <option id="encontreServico" value=”equipamentos”>Equipamentos</option>
+                    <option id="encontreServico" value=”manutenção”>Manutenção</option>
+                </select> -->
+                </div><br>
+                <div class="col-lg-1">
+                    <button type="submit" class="btn encontreBotao">Encontre</button>
                 </div>
             </div>
-        </section>
-
-
-
-        <section class="container">
-            <!-- titulo da pagina -->
-            <h1 id="encontreTopo" class="mt-5 text-center encontreTitulo">Encontre tudo o que você precisa para aproveitar o mar:</h1>
-
-            <!-- campos de busca -->
-            <div class="row">
-                <form action="/action_page.php" class="form-inline col-lg-11">
-                    <div class="col-lg-8 mt-5">
-                            <input class="form-control w-100" type="search" placeholder="Onde?"
-                                aria-label="Search">
-                    </div>
-                    <div class="mt-5 col-lg-3">
-                        <select class="form-control w-100" name="encontre">
-                            <option disabled selected>O que?</option>
-                            <option value=”equipamentos”>Equipamentos</option>
-                            <option value=”manutenção”>Manutenção</option>
-                            <option value=”surfshop”>Surf Shop</option>
-                            <option value=”acomodacao”>Acomodação</option>
-                            <option value=”carona”>Carona</option>
-                        </select>
-                    </div>
-                    <div class="col-lg-1 mt-5">
-                        <button type="submit" class="btn encontreBotao">Encontre</button>
-                    </div>
-                </form>
-            </div>
+            <!-- </div> -->
+            <!-- </section> -->
 
             <!-- mapa do google -->
-            <div class="container mt-5 mb-5 p-0">
-                <img src="imagens/img-encontre/mapa.png" class="img-fluid" alt="mapa">
+            <!-- <section class="container"> -->
+            <div class="col-lg-7 col-md-7">
+                <img src="imagens/img-encontre/mapa.png" alt="mapa" class="encontreMapa">
             </div>
-            </div>
+    </section>
 
-            <!-- sessao vinculada a busca realizada e ofertas no desapego -->
-        </section>
-        <div class="container mb-5 encontreMobile">
-            <h2 class="encontreTitulo">Itens a venda nesta área:</h2>
-            <div class="row justify-content-between mt-5">
-                <a href="./desapegoOferta.php"><img class="encontredesapego" src="imagens/img-encontre/venda1.png" alt="desapego"></a>
-                <a href="./desapegoOferta.php"><img class="encontredesapego" src="imagens/img-encontre/venda1.png" alt="desapego"></a>
-                <a href="./desapegoOferta.php"><img class="encontredesapego" src="imagens/img-encontre/venda1.png" alt="desapego"></a>
-            </div>
-        </div>
+    <!-- botao topo -->
+    <div class="container text-center mt-5 mb-5">
+        <a href="#encontreTopo" class="btn encontreBotao">Topo</a>
+    </div>
 
-        <!-- botao topo -->
-        <div class="container text-center mt-5 mb-5">
-            <a href="#encontreTopo" class="btn encontreBotao">Topo</a>
-        </div>
-
-@endsection
+    @endsection
