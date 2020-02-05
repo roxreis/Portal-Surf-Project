bee756d9fa2dbae3565f2dddecb9fb8c571056df
Caio Alves <caiojuodzevicius@gmail.com>
2020-02-05 00:22:36 -0300

    Update route desapego

--- routes/web.php
+++ routes/web.php
@@ -30,13 +30,13 @@ Route::get('/desapego',"DesapegoController@viewDesapego");
 // Route::get('/desapegoOfertasUsuario',"DesapegoController@viewDesapegoOfertasUsuario");
 Route::get('/desapegoOfertaIndividual',"DesapegoController@viewDesapegoOfertaIndividual");
 
-// vou informar que a rota abaixo precisa de usuario logado, informando o middleware que verifica isso, usando o checkuser que cadastramos dentro do Kernel. Poderiamos usar apenas nesta porque as outras paginas teoricamente so seriam acessadas apos esta, mas por seguranca se coloca em todas. 
-Route::get('/desapegoCadastroOferta', 'CrudDesapegoController@viewDesapegoCadastroOferta')->middleware('checkuser');
-Route::post('/desapegoCadastroOferta', 'CrudDesapegoController@create');
-Route::get('/desapegoEditarOferta/{id?}', 'CrudDesapegoController@edit');
-// rota para botao atualizar na view, para de fato salvar os dados atualizados no banco de dados.
-Route::post('/desapegoEditarOferta', 'CrudDesapegoController@update');
-Route::get('/desapegoOfertasUsuario/{id?}', 'CrudDesapegoController@delete');
+// // vou informar que a rota abaixo precisa de usuario logado, informando o middleware que verifica isso, usando o checkuser que cadastramos dentro do Kernel. Poderiamos usar apenas nesta porque as outras paginas teoricamente so seriam acessadas apos esta, mas por seguranca se coloca em todas. 
+// Route::get('/desapegoCadastroOferta', 'CrudDesapegoController@viewDesapegoCadastroOferta')->middleware('checkuser');
+// Route::post('/desapegoCadastroOferta', 'CrudDesapegoController@create');
+// Route::get('/desapegoEditarOferta/{id?}', 'CrudDesapegoController@edit');
+// // rota para botao atualizar na view, para de fato salvar os dados atualizados no banco de dados.
+// Route::post('/desapegoEditarOferta', 'CrudDesapegoController@update');
+// Route::get('/desapegoOfertasUsuario/{id?}', 'CrudDesapegoController@delete');
 
 
 Route::get('/encontre', 'EncontreController@viewEncontre');
