0396a7cc640117087255aa94639f51107f197a0f
Caio Alves <caiojuodzevicius@gmail.com>
2020-02-04 23:39:24 -0300

    Update migration/model desapego

--- app/Desapego.php
+++ app/Desapego.php
@@ -23,6 +23,12 @@ class Desapego extends Model{
         protected $guarded = ['id', 'created_at', 'update_at'];
         protected $table = 'desapego_produtos';
 
+        
+        public function user(){
+            return $this->belongsTo('App\User');
+            // para vincular a criacao do produto ao id do usuario que o cadastrou.
+        }
+    
     
     }
 
